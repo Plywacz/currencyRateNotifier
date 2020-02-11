c1cf000036cbc6835e65bb0911a22f2df43f4771
Plywacz <=>
2020-02-11 13:23:55 +0100

    exposed new notifying functionality

--- src/main/java/m/plywacz/exchangeratesapi/controllers/NotificationController.java
+++ src/main/java/m/plywacz/exchangeratesapi/controllers/NotificationController.java
@@ -7,12 +7,10 @@ Date: 10.02.2020
 import m.plywacz.exchangeratesapi.dto.NotificationDto;
 import m.plywacz.exchangeratesapi.model.Notification;
 import m.plywacz.exchangeratesapi.services.NotificationService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/notifications")
@@ -26,5 +24,15 @@ public class NotificationController {
     @PostMapping
     public Notification addNotification(@RequestBody @Valid NotificationDto notificationDto) {
         return notificationService.addNotification(notificationDto);
+    }
+
+    @GetMapping
+    public List<Notification> getAllNotifications() {
+        return notificationService.getAllNotification();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteNotification(@PathVariable Long id) {
+        notificationService.deleteNotification(id);
     }
 }
